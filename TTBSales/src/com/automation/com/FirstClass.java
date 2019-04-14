072e6ca1ad5c3bf9d4c11f19fb4fdb5205aab986
anirudhdebbadiTest <47914431+anirudhdebbadiTest@users.noreply.github.com>
2019-04-14 20:58:31 +0530

    Update FirstClass.java
    vv

--- TTBSales/src/com/automation/com/FirstClass.java
+++ TTBSales/src/com/automation/com/FirstClass.java
@@ -5,11 +5,16 @@ public class FirstClass {
 	
 	public void abc()
 	{	
-		for(int i=15;i>10;i--)
+		for(int i=15;i<220;i--)
 		{
 			System.out.println("a");
 			System.out.println("b");
 			System.out.println("c");
+			System.out.println("c");
+			System.out.println("c");
+			System.out.println("c");
+			System.out.println("c");
+			
 			System.out.println("d");
 			System.out.println("e");
 			System.out.println("anirudh");
