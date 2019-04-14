c107ae7faf8678ed0ffefba5e96a3e76d2eed132
anirudhdebbadi09 <anirudhdebbadi09@gmail.com>
2019-04-14 20:38:01 +0530

    first commit

---
+++ TTBSales/src/com/automation/com/FirstClass.java
@@ -0,0 +1,18 @@
+package com.automation.com;
+
+public class FirstClass {
+	
+	
+	public void abc()
+	{	
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("a");
+			System.out.println("b");
+			System.out.println("c");
+			System.out.println("d");
+			System.out.println("e");
+		}
+	}
+
+}
