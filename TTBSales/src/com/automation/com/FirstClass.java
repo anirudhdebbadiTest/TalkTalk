4142c4f12ae3092e291ed6d31227aeedbe4a089b
anirudhdebbadiTest <47914431+anirudhdebbadiTest@users.noreply.github.com>
2019-04-14 20:53:40 +0530

    Update FirstClass.java
    committing in remote

--- TTBSales/src/com/automation/com/FirstClass.java
+++ TTBSales/src/com/automation/com/FirstClass.java
@@ -5,18 +5,14 @@ public class FirstClass {
 	
 	public void abc()
 	{	
-		for(int i=0;i<10;i++)
+		for(int i=15;i>10;i--)
 		{
 			System.out.println("a");
 			System.out.println("b");
 			System.out.println("c");
 			System.out.println("d");
 			System.out.println("e");
-			System.out.println("f");
-			System.out.println("g");
-			System.out.println("h");
-			System.out.println("i");
-			System.out.println("j");
+			System.out.println("anirudh");
 		}
 	}
 
